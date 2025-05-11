68faa5b25f98ad5bb760f414c0636c2390b57969
desmonHak <61366533+desmonHak@users.noreply.github.com>
2025-05-11 15:24:24 +0200

    creando ejecutable minimo

---
+++ include/CreateELF.h
@@ -0,0 +1,8 @@
+//
+// Created by desmon0xff on 11/05/2025.
+//
+
+#ifndef CREATEELF_H
+#define CREATEELF_H
+
+#endif //CREATEELF_H
---
+++ src/CreateELF.c
@@ -0,0 +1,5 @@
+//
+// Created by desmon0xff on 11/05/2025.
+//
+
+#include "CreateELF.h"
